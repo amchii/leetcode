37638d3f53b76d3c3a4a833000108410690ebd0b
caoheng <finethankuandyou@gmail.com>
2020-07-17 17:19:34 +0800

    add-two-numbers

---
+++ add-two-numbers/solution.py
@@ -0,0 +1,113 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def add_and_carry(self, *val):
+        e = sum(val)
+        if e >= 10:
+            e %= 10
+            return e, 1
+        return e, 0
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        result = ListNode(0)
+        head = result
+        carry = 0
+        while l1 or l2:
+            if not l1:
+                x = 0
+                y = l2.val
+            else:
+                x = l1.val
+                y = 0 if not l2 else l2.val
+            e, next_carry = self.add_and_carry(x, y, carry)
+            head.next = ListNode(e)
+            head = head.next
+            carry = next_carry
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if carry:
+            head.next = ListNode(carry)
+        return result.next
+
+
+class StupidSolution:
+    def add_and_carry(self, *vals):
+        e = sum(vals)
+        carry = 0
+        if e >= 10:
+            e %= 10
+            carry = 1
+        return e, carry
+
+    def ListNodeToList(self, l: ListNode):
+        res = []
+        while l is not None:
+            res.append(l.val)
+            l = l.next
+        return res
+
+    def ListToListNode(self, l: list):
+        res = ListNode(0)
+        head = res
+        for i in l:
+            head.next = ListNode(i)
+            head = head.next
+        return res.next
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        list_l1 = self.ListNodeToList(l1)
+        list_l2 = self.ListNodeToList(l2)
+        res = []
+        len1 = len(list_l1)
+        len2 = len(list_l2)
+        if len1 > len2:
+            max_len = len1
+            list_l2.extend([0] * (len1 - len2))
+        else:
+            max_len = len2
+            list_l1.extend([0] * (len2 - len1))
+        carry = 0
+        for i in range(max_len):
+            e1, e2 = list_l1[i], list_l2[i]
+            e, next_carry = self.add_and_carry(e1, e2, carry)
+            res.append(e)
+            carry = next_carry
+        if carry:
+            res.append(carry)
+        return self.ListToListNode(res)
+
+
+def ListNodeToList(l: ListNode):
+    res = []
+    while l is not None:
+        res.append(l.val)
+        l = l.next
+    return res
+
+
+def ListToListNode(l: list):
+    res = ListNode(0)
+    head = res
+    for i in l:
+        head.next = ListNode(i)
+        head = head.next
+    return res.next
+
+
+def printListNode(l):
+    print(l.val, end=' ')
+    if l.next:
+        printListNode(l.next)
+
+
+if __name__ == '__main__':
+    l1 = ListToListNode([3, 5, 6])
+    l2 = ListToListNode([4, 5, 7, 8])
+    printListNode(Solution().addTwoNumbers(l1, l2))
